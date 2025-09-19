cea000f610023f4cd6f372b9e596994bde120641
Vsathish2002 <vsathish22120@gmail.com.>
2025-09-19 16:34:12 +0530

    Create Forms

--- String.php
+++ String.php
@@ -3,23 +3,23 @@ $str = "Hello World";
 echo "Length: " . strlen($str) . "<br>"; 
 
 
-echo "Words: " . str_word_count($str) . "\n"; 
+echo "Words: " . str_word_count($str) .  "<br>"; 
 
-echo "Reversed: " . strrev($str) . "\n"; 
+echo "Reversed: " . strrev($str) .  "<br>"; 
 
-echo "Position of 'World': " . strpos($str, "World") . "\n"; 
+echo "Position of 'World': " . strpos($str, "World") .  "<br>"; 
 
-echo "Last position of 'l': " . strrpos($str, "l") . "\n"; 
+echo "Last position of 'l': " . strrpos($str, "l") .  "<br>"; 
 
-echo "Uppercase: " . strtoupper($str) . "\n"; 
+echo "Uppercase: " . strtoupper($str) .  "<br>"; 
 
-echo "Lowercase: " . strtolower($str) . "\n"; 
+echo "Lowercase: " . strtolower($str) .  "<br>"; 
 
-echo "Ucfirst: " . ucfirst(strtolower($str)) . "\n"; 
+echo "Ucfirst: " . ucfirst(strtolower($str)) .  "<br>"; 
 
-echo "Lcfirst: " . lcfirst($str) . "\n"; 
+echo "Lcfirst: " . lcfirst($str) .  "<br>"; 
 
-echo "Ucwords: " . ucwords(strtolower($str)) . "\n"; 
+echo "Ucwords: " . ucwords(strtolower($str)) .  "<br>"; 
 
 $str2 = "   PHP   ";
 echo "Trimmed: '" . trim($str2) . "'\n"; 
@@ -28,40 +28,46 @@ echo "Ltrim: '" . ltrim($str2) . "'\n";
 
 echo "Rtrim: '" . rtrim($str2) . "'\n"; 
 
-echo str_replace("World", "PHP", $str) . "\n"; 
+echo str_replace("World", "PHP", $str) .  "<br>"; 
 
-echo str_ireplace("world", "PHP", $str) . "\n"; 
+echo str_ireplace("world", "PHP", $str) .  "<br>"; 
 
-echo substr($str, 6) . "\n"; 
-echo substr($str, 0, 5) . "\n"; 
+echo substr($str, 6) .  "<br>"; 
+echo substr($str, 0, 5) .  "<br>"; 
+
+
 
 $str3 = "apple,banana,orange";
 print_r(explode(",", $str3));
 
-
 $arr = ["PHP", "is", "fun"];
-echo implode(" ", $arr) . "\n"; 
+echo implode(" ", $arr) .  "<br>"; 
 
-echo strcmp("Hello", "hello") . "\n"; 
 
-echo strcasecmp("Hello", "hello") . "\n"; /
+
+echo strcmp("Hello", "hello") .  "<br>"; 
+
+echo strcasecmp("Hello", "hello") .  "<br>"; 
+
+
 $str4 = "It's PHP";
-echo addslashes($str4) . "\n";
 
-echo stripslashes("It\'s PHP") . "\n"; 
+echo addslashes($str4) .  "<br>";
+echo stripslashes("It\'s PHP") .  "<br>"; 
+
 
 $str5 = "<b>Bold</b>";
-echo htmlentities($str5) . "\n"; 
+echo htmlentities($str5) .  "<br>"; 
 
-echo html_entity_decode("&lt;b&gt;Bold&lt;/b&gt;") . "\n";
+echo html_entity_decode("&lt;b&gt;Bold&lt;/b&gt;") .  "<br>";
 
-$str6 = "Hello\nWorld";
+$str6 = "Hello \n World";
 echo nl2br($str6); 
 
-echo str_repeat("PHP ", 3) . "\n"; 
+echo str_repeat("PHP ", 3) .  "<br>"; 
 
 $str7 = "PHP is a popular scripting language.";
-echo wordwrap($str7, 10, "\n") . "\n";
+echo wordwrap($str7, 10,  "<br>") .  "<br>";
 
 
 ?>
